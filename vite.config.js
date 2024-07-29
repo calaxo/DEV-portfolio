f3aba42c2d428f20ced1422c0616fd75dac34cec
axel <36140542+calaxo@users.noreply.github.com>
2024-07-29 10:45:48 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -17,7 +17,6 @@ export default defineConfig({
       '@assets': path.resolve(__dirname, './src/assets'),
       '@components': path.resolve(__dirname, './src/components'),
         '@pages': path.resolve(__dirname, './src/pages'),
-        '@styles': path.resolve(__dirname, './src/styles'),
     },
   },
   build: {
