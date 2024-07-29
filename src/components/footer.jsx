f7828dca0aef04d13fbb5cca59da6d62364613a6
axel <36140542+calaxo@users.noreply.github.com>
2024-07-29 10:47:19 +0200

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <footer className="py-4 text-white bg-gray-800">
             <div className="container flex justify-center mx-auto">
-                <p className="text-sm">&copy; 2022 My Portfolio. All rights reserved.</p>
+                <p className="text-sm">&copy; 2022 My Website. All rights reserved.</p>
             </div>
         </footer>
     );
